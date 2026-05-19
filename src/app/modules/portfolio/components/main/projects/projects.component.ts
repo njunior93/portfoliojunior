5d80b37caf6ad2d65b87b9fe3712c01932354abe
Natanael junior <138621051+njunior93@users.noreply.github.com>
2026-05-19 02:40:00 -0300

    Add project details to ProjectsComponent

--- src/app/modules/portfolio/components/main/projects/projects.component.ts
+++ src/app/modules/portfolio/components/main/projects/projects.component.ts
@@ -13,6 +13,21 @@ export class ProjectsComponent {
 
   public arrayProjects = signal<IPost[]>([
     {
+      texto: 'Aqui nasceu um "FullStack"',
+      textoDialog: 'O Agenda Fácil nasceu como um projeto para fortalecer meus conhecimentos como desenvolvedor fullstack. Quis criar algo que realmente simulasse um sistema usado no dia a dia, então desenvolvi uma aplicação completa de agendamentos, gerenciamento de clientes e serviços, utilizando React, TypeScript, NestJS e TypeORM. Durante o desenvolvimento, consegui praticar desde a criação da interface até regras de negócio, API e banco de dados.',
+      src: 'assets/posts/agenda-facil.jpg',
+      data: '19 Mai 2026',
+      projeto: [
+        {
+          href: 'https://github.com/njunior93/agenda-facil-front_end',
+          deploy: 'https://agenda-facil-ten.vercel.app',
+        },
+      ],
+      alt: 'Imagem do projeto Agenda Facil',
+      iconeLink:
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z" clip-rule="evenodd"></path></svg>',
+    },
+    {
       texto: 'MEU GRANDE PROJETO',
       textoDialog: 'A ideia surgiu numa conversa com um amigo: fazer entregas de hortifruti para fast foods pela cidade. E pensei: "consigo fazer um sistema pra isso", E fiz! Desenvolvi uma aplicação completa com gerenciamento de estoque, criação de pedidos e geração de relatórios, tudo usando <b>React, TypeScript e NestJS + TypeORM</b>. O projeto já está quase finalizado (uns 95%) e ficou bem próximo de um sistema real, pensado para uso no dia a dia. Então é só clicar em uma das opções abaixo e conferir',
       src: 'assets/posts/projeto_hotifacil.jpg',
